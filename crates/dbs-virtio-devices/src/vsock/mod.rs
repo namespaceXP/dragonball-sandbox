ef28423a592dbaef8107fdd67dfb0ba183d91149
Beiyue <beiyue@linux.alibaba.com>
2022-04-07 20:20:09 +0800

    dbs-virtio-devices: add vsock packet.
    Add VsockPacket, which provides a thin wrapper over the buffers used by
    vsock.

--- crates/dbs-virtio-devices/src/vsock/mod.rs
+++ crates/dbs-virtio-devices/src/vsock/mod.rs
@@ -6,6 +6,13 @@
 // found in the THIRD-PARTY file.
 
 pub mod backend;
+mod packet;
+
+use std::os::unix::io::AsRawFd;
+
+use vm_memory::GuestMemoryError;
+
+use self::packet::VsockPacket;
 
 mod defs {
     /// RX queue event: the driver added available buffers to the RX queue.
@@ -88,6 +95,72 @@ pub enum VsockError {
     /// vsock backend error
     #[error("Vsock backend error: {0}")]
     Backend(#[source] std::io::Error),
+    /// The vsock data/buffer virtio descriptor is expected, but missing.
+    #[error("The vsock data/buffer virtio descriptor is expected, but missing")]
+    BufDescMissing,
+    /// The vsock data/buffer virtio descriptor length is smaller than expected.
+    #[error("The vsock data/buffer virtio descriptor length is smaller than expected")]
+    BufDescTooSmall,
+    /// Chained GuestMemory error.
+    #[error("Chained GuestMemory error: {0}")]
+    GuestMemory(#[source] GuestMemoryError),
+    /// Bounds check failed on guest memory pointer.
+    #[error("Bounds check failed on guest memory pointer, addr: {0}, size: {1}")]
+    GuestMemoryBounds(u64, usize),
+    /// The vsock header descriptor length is too small.
+    #[error("The vsock header descriptor length {0} is too small")]
+    HdrDescTooSmall(u32),
+    /// The vsock header `len` field holds an invalid value.
+    #[error("The vsock header `len` field holds an invalid value {0}")]
+    InvalidPktLen(u32),
+    /// A data fetch was attempted when no data was available.
+    #[error("A data fetch was attempted when no data was available")]
+    NoData,
+    /// A data buffer was expected for the provided packet, but it is missing.
+    #[error("A data buffer was expected for the provided packet, but it is missing")]
+    PktBufMissing,
+    /// Encountered an unexpected write-only virtio descriptor.
+    #[error("Encountered an unexpected write-only virtio descriptor")]
+    UnreadableDescriptor,
+    /// Encountered an unexpected read-only virtio descriptor.
+    #[error("Encountered an unexpected read-only virtio descriptor")]
+    UnwritableDescriptor,
 }
 
 type Result<T> = std::result::Result<T, VsockError>;
+
+/// A passive, event-driven object, that needs to be notified whenever an
+/// epoll-able event occurs. An event-polling control loop will use
+/// `get_polled_fd()` and `get_polled_evset()` to query the listener for the
+/// file descriptor and the set of events it's interested in. When such an event
+/// occurs, the control loop will route the event to the listener via
+/// `notify()`.
+pub trait VsockEpollListener: AsRawFd {
+    /// Get the set of events for which the listener wants to be notified.
+    fn get_polled_evset(&self) -> epoll::Events;
+
+    /// Notify the listener that one ore more events have occured.
+    fn notify(&mut self, evset: epoll::Events);
+}
+
+/// Any channel that handles vsock packet traffic: sending and receiving
+/// packets. Since we're implementing the device model here, our responsibility
+/// is to always process the sending of packets (i.e. the TX queue). So, any
+/// locally generated data, addressed to the driver (e.g. a connection response
+/// or RST), will have to be queued, until we get to processing the RX queue.
+///
+/// Note: `recv_pkt()` and `send_pkt()` are named analogous to `Read::read()`
+///       and `Write::write()`, respectively. I.e. - `recv_pkt()` will read data
+///       from the channel, and place it into a packet; and - `send_pkt()` will
+///       fetch data from a packet, and place it into the channel.
+pub trait VsockChannel {
+    /// Read/receive an incoming packet from the channel.
+    fn recv_pkt(&mut self, pkt: &mut VsockPacket) -> Result<()>;
+
+    /// Write/send a packet through the channel.
+    fn send_pkt(&mut self, pkt: &VsockPacket) -> Result<()>;
+
+    /// Checks weather there is pending incoming data inside the channel,
+    /// meaning that a subsequent call to `recv_pkt()` won't fail.
+    fn has_pending_rx(&self) -> bool;
+}
